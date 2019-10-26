9932235b3d1f456f7fca3aaf10fa7b840a4b2e82
Gerardo Zeledon <gerardo.zeledon14@gmail.com>
2019-10-26 00:55:47 -0600

    Delete sql package in main.go

---
+++ golang/main/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+
+	"github.com/Gezele14/Proyecto2-SOA/golang/graphql"
+)
+
+type Server struct {
+	GqlSchema *graphql.Schema
+}
+
+type reqBody struct {
+	Query string `json:"query"`
+}
+
+func (s *Server) GraphQL() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Body == nil {
+			http.Error(w, "Must provide graphql query in request body", 400)
+			return
+		}
+
+		var rBody reqBody
+
+		err := json.NewDecoder(r.Body).Decode(&rBody)
+
+		if err != nil {
+			http.Error(w, "Error parsing JSON request body", 400)
+		}
+
+		result := graphql.ExecuteQuery(rBody.Query, *s.GqlSchema)
+
+		render.JSON(w, r, result)
+	}
+}
+
+func initializeAPI() (*chi.Mux, *sql.Db) {
+
+	router := chi.NewRouter()
+	db, err := sql.New("root:123456@tcp(192.168.219.93:3306)/restDB")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.DefaultCompress,
+		middleware.StripSlashes,
+		middleware.Recoverer,
+	)
+
+	return router, db
+}
+
+func main() {
+	router, db := initializeAPI()
+	defer db.Close()
+	log.Fatal(http.ListenAndServe(":8100", router))
+}
