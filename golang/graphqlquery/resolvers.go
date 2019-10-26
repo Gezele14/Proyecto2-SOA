e0326d0479c241015a42f51b348e4376c03d730c
Gerardo Zeledon <gerardo.zeledon14@gmail.com>
2019-10-26 04:59:34 -0600

    Running go server with GraphQL

---
+++ golang/graphqlquery/resolvers.go
@@ -0,0 +1,65 @@
+package graphqlquery
+
+import (
+	"strconv"
+
+	"../database"
+	"github.com/graphql-go/graphql"
+)
+
+// Resolver struct holds a connection to our database
+type Resolver struct {
+	db *database.Db
+}
+
+// OrderResolver resolves our user query through a db call to GetOrders
+func (r *Resolver) OrderResolver(p graphql.ResolveParams) (interface{}, error) {
+	// Strip the name from arguments and assert that it's a string
+	idRestaurant, ok := p.Args["idRestaurant"].(int)
+	idClient, ok1 := p.Args["idClient"].(int)
+
+	if ok {
+		orders := r.db.GetOrders(strconv.Itoa(idRestaurant), "idRestaurant")
+		return orders, nil
+	} else if ok1 {
+		orders := r.db.GetOrders(strconv.Itoa(idClient), "idClient")
+		return orders, nil
+	}
+
+	orders := r.db.GetOrders("", "none")
+
+	return orders, nil
+}
+
+// OrderCreator resolves our user query through a db call to CreateOrder
+func (r *Resolver) OrderCreator(p graphql.ResolveParams) (interface{}, error) {
+	// Strip the name from arguments and assert that it's a string
+	orderDate, _ := p.Args["orderDate"].(string)
+	details, _ := p.Args["details"].(string)
+	idRestaurant, _ := p.Args["idRestaurant"].(int)
+
+	order := r.db.CreateOrder(orderDate, details, idRestaurant)
+
+	return order, nil
+}
+
+// OrderDeleter resolves our user query through a db call to DeleteOrder
+func (r *Resolver) OrderDeleter(p graphql.ResolveParams) (interface{}, error) {
+	// Strip the name from arguments and assert that it's a string
+	idOrder, _ := p.Args["idOrder"].(int)
+
+	rowsAffected := r.db.DeleteOrder(idOrder)
+
+	return rowsAffected, nil
+}
+
+// OrderUpdater resolves our user query through a db call to UpdateOrder
+func (r *Resolver) OrderUpdater(p graphql.ResolveParams) (interface{}, error) {
+	// Strip the name from arguments and assert that it's a string
+	idOrder, _ := p.Args["idOrder"].(int)
+	idRestaurants, _ := p.Args["idRestaurants"].(int)
+
+	rowsAffected := r.db.UpdateOrder(idOrder, idRestaurants)
+
+	return rowsAffected, nil
+}
--- golang/main/main.go
+++ golang/main/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
+	"github.com/graphql-go/graphql"
 
-	"github.com/Gezele14/Proyecto2-SOA/golang/graphql"
+	"../database"
+	"../graphqlquery"
 )
 
 type Server struct {
@@ -20,44 +23,77 @@ type reqBody struct {
 	Query string `json:"query"`
 }
 
+// GraphQL returns an http.HandlerFunc for our /graphql endpoint
 func (s *Server) GraphQL() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		// Check to ensure query was provided in the request body
 		if r.Body == nil {
 			http.Error(w, "Must provide graphql query in request body", 400)
 			return
 		}
 
 		var rBody reqBody
-
+		// Decode the request body into rBody
 		err := json.NewDecoder(r.Body).Decode(&rBody)
-
 		if err != nil {
 			http.Error(w, "Error parsing JSON request body", 400)
 		}
+		// Execute graphql query
+		result := graphqlquery.ExecuteQuery(rBody.Query, *s.GqlSchema)
 
-		result := graphql.ExecuteQuery(rBody.Query, *s.GqlSchema)
-
+		// render.JSON comes from the chi/render package and handles
+		// marshalling to json, automatically escaping HTML and setting
+		// the Content-Type as application/json.
 		render.JSON(w, r, result)
 	}
 }
 
-func initializeAPI() (*chi.Mux, *sql.Db) {
+// GraphQL returns an http.HandlerFunc for our /graphql endpoint
+func (s *Server) ServiceWelcome() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "orders-service")
+	}
+}
 
+func initializeAPI() (*chi.Mux, *database.Db) {
+	// Create a new router
 	router := chi.NewRouter()
-	db, err := sql.New("root:123456@tcp(192.168.219.93:3306)/restDB")
+
+	// Create a new connection to our pg database
+	db, err := database.New("root:123456@tcp(192.168.219.93:3306)/restDB")
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Create our root query for graphql
+	rootQuery := graphqlquery.NewRoot(db)
+	// Create a new graphql schema, passing in the the root query
+	sc, err := graphql.NewSchema(
+		graphql.SchemaConfig{Query: rootQuery.Query, Mutation: rootQuery.Mutation},
+	)
+	if err != nil {
+		fmt.Println("Error creating schema: ", err)
+	}
+
+	// Create a server struct that holds a pointer to our database as well
+	// as the address of our graphql schema
+	s := Server{
+		GqlSchema: &sc,
+	}
+
+	// Add some middleware to our router
 	router.Use(
-		render.SetContentType(render.ContentTypeJSON),
-		middleware.Logger,
-		middleware.DefaultCompress,
-		middleware.StripSlashes,
-		middleware.Recoverer,
+		render.SetContentType(render.ContentTypeJSON), // set content-type headers as application/json
+		middleware.Logger,          // log api request calls
+		middleware.DefaultCompress, // compress results, mostly gzipping assets and json
+		middleware.StripSlashes,    // match paths with a trailing slash, strip it, and continue routing through the mux
+		middleware.Recoverer,       // recover from panics without crashing server
 	)
 
+	// Create the graphql route with a Server method to handle it
+	router.Get("/orders", s.ServiceWelcome())
+	router.Post("/orders/graphql", s.GraphQL())
 	return router, db
 }
 
